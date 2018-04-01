89d3d80e285c8802998396346b572d1635642cf6
fyh <1913068672@qq.com>
2018-04-01 23:56:23 +0800

    delete change

--- resources/views/wocao/delete_article.blade.php
+++ resources/views/wocao/delete_article.blade.php
@@ -8,7 +8,7 @@
     {!! Form::open(['action' => "Wocao\Article\DeleteArticleController@post",'method' => 'post']) !!}
     {{csrf_field()}}
 
-    <select class="selectpicker" name="a_type">
+    <select class="selectpicker" id="tp" name="a_type">
         <option value="中心简介">中心简介</option>
         <option value="政策法规">政策法规</option>
         <option value="规章制度">规章制度</option>
@@ -18,31 +18,37 @@
         <option value="生活常识">生活常识</option>
         <option value="公告">公告</option>
     </select>
-    <input class="btn btn-default" type="submit" value="查看"/>
+    <input class="btn btn-default" onclick="check()" type="submit" value="查看"/>
+    <script>
+    function check(){
+    var s = document.getElementById('a')
+    console.log("sdf:"+s.value)
+    if(s.value==''){
+      alert("请选择类型:")
+      return false;
+    }
+
+  }
+    </script>
     {!! Form::close() !!}
     {{--@if(isset($flag) && $flag==true)--}}
-    @if(true)
-        <div class="panel-body">
-            <ul class="list-group">
-                <?php $i=1;?>
-                @foreach($data as $v)
-                    <?php $path = url('delete_article/'.$v->a_id)?>
-                    <a class="list-group-item" href='#' onclick="doDelete(this,'{{$path}}');">
-                        <span class="badge">{{'创建时间:'.$v->created_at}}</span>
-                        {{$v->a_title}}
-                    </a>
-                    <?php $i++?>
-                @endforeach
-
-            </ul>
-            <!-- 分页栏 -->
-            <nav aria-label="Page navigation">
-                {{$data->links()}}
-            </nav>
-        </div>
-    @else
-        <h1 class="well">没有选定文章类型无法显示信息</h1>
-    @endif
+    <div class="panel-body">
+        <ul class="list-group">
+            <?php $i=1;?>
+            @foreach($data as $v)
+                <?php $path = url('delete_article/'.$v->a_id)?>
+                <a class="list-group-item" href='#' onclick="doDelete(this,'{{$path}}');">
+                    <span class="badge">{{'创建时间:'.$v->created_at}}</span>
+                    {{$v->a_title}}
+                </a>
+                <?php $i++?>
+            @endforeach
+        </ul>
+        <!-- 分页栏 -->
+        <nav aria-label="Page navigation">
+            {{$data->links()}}
+        </nav>
+    </div>
 
 </div>
 </body>
